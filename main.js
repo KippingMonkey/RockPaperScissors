53aa8576e80f0f87cadd7db7d1e522c6a79d8a8d
Jonas <jonas@kippingmonkey.se>
2021-10-22 00:10:04 +0200

    make game rounds. Add event to reset-btn

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//global variabels
+let header = document.querySelector("h1");
+let img1 = document.getElementById("img-left");
+let img2 = document.getElementById("img-right");
+let values = ["rock", "paper", "scissors"];
+let rand1 = Math.floor(Math.random() * 3);
+let rand2 = Math.floor(Math.random() * 3);
+let resetbtn = document.getElementById("reset-btn");
+
 // Check if the page has been loaded before by looking for data saved in sessionStorage
 if (sessionStorage.getItem("firstLoad")) {
   playGame();
@@ -7,14 +16,6 @@ else {
   sessionStorage.setItem("firstLoad", true);
 }
 
-//global variabels
-let header = document.querySelector("h1");
-let img1 = document.getElementById("img-left");
-let img2 = document.getElementById("img-right");
-let values = ["rock", "paper", "scissors"];
-let rand1 = Math.floor(Math.random() * 3);
-let rand2 = Math.floor(Math.random() * 3);
-
 function playGame() {
   let randImgLeft = `images/${values[rand1]}.png`;
   let randImgRight = `images/${values[rand2]}.png`;
@@ -22,7 +23,24 @@ function playGame() {
   img1.setAttribute("src", randImgLeft);
   img2.setAttribute("src", randImgRight);
 
-  changeHeader();
+  if (rand1 == 0 && rand2 == 2) {
+    header.textContent = "Player One Wins!";
+  } else if (rand1 == 2 && rand2 == 0) {
+    header.textContent = "Player Two Wins!";
+  } else if (rand1 == 1 && rand2 == 0) {
+    header.textContent = "Player One Wins!";
+  } else if (rand1 == 0 && rand2 == 1) {
+    header.textContent = "Player Two Wins!";
+  } else if (rand1 == 2 && rand2 == 1) {
+    header.textContent = "Player One Wins!";
+  } else if (rand1 == 1 && rand2 == 2) {
+    header.textContent = "Player Two Wins!";
+  } else {
+    header.textContent = "Draw!";
+  }
 }
-//Change header according to outcome (ie. Rock breaks Scissors)
-function changeHeader() {}
+
+resetbtn.addEventListener("click", () => {
+  sessionStorage.removeItem("firstLoad", true);
+  location.reload();
+});
