838446ed92e53a37d735b749880969a2782fbc34
Jonas <jonas@kippingmonkey.se>
2021-10-22 10:00:57 +0200

    work on score system with sessionStorage

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let values = ["rock", "paper", "scissors"];
 let rand1 = Math.floor(Math.random() * 3);
 let rand2 = Math.floor(Math.random() * 3);
 let resetbtn = document.getElementById("reset-btn");
+let score = document.querySelector(".score");
 
 // Check if the page has been loaded before by looking for data saved in sessionStorage
 if (sessionStorage.getItem("firstLoad")) {
@@ -14,6 +15,8 @@ if (sessionStorage.getItem("firstLoad")) {
 //if it loading for the first time, save "firstLoad" in sessionStorage
 else {
   sessionStorage.setItem("firstLoad", true);
+  sessionStorage.setItem("p1Score", 0);
+  sessionStorage.setItem("p2Score", 0);
 }
 
 function playGame() {
@@ -25,22 +28,52 @@ function playGame() {
 
   if (rand1 == 0 && rand2 == 2) {
     header.textContent = "Player One Wins!";
+    var newScore = Number(sessionStorage.getItem("p1Score")) + 1;
+    sessionStorage.setItem("p1Score", newScore);
+    score.textContent = `Score ${newScore} - ${Number(
+      sessionStorage.getItem("p2Score")
+    )}`;
   } else if (rand1 == 2 && rand2 == 0) {
     header.textContent = "Player Two Wins!";
+    var newScore = Number(sessionStorage.getItem("p2Score")) + 1;
+    score.textContent = `${Number(
+      sessionStorage.getItem("p1Score")
+    )} - ${newScore}`;
   } else if (rand1 == 1 && rand2 == 0) {
     header.textContent = "Player One Wins!";
+    var newScore = Number(sessionStorage.getItem("p1Score")) + 1;
+    sessionStorage.setItem("p1Score", newScore);
+    score.textContent = `Score ${newScore} - ${Number(
+      sessionStorage.getItem("p2Score")
+    )}`;
   } else if (rand1 == 0 && rand2 == 1) {
     header.textContent = "Player Two Wins!";
+    var newScore = Number(sessionStorage.getItem("p2Score")) + 1;
+    score.textContent = `${Number(
+      sessionStorage.getItem("p1Score")
+    )} - ${newScore}`;
   } else if (rand1 == 2 && rand2 == 1) {
     header.textContent = "Player One Wins!";
+    var newScore = Number(sessionStorage.getItem("p1Score")) + 1;
+    sessionStorage.setItem("p1Score", newScore);
+    score.textContent = `Score ${newScore} - ${Number(
+      sessionStorage.getItem("p2Score")
+    )}`;
   } else if (rand1 == 1 && rand2 == 2) {
     header.textContent = "Player Two Wins!";
+    var newScore = Number(sessionStorage.getItem("p2Score")) + 1;
+    score.textContent = `${Number(
+      sessionStorage.getItem("p1Score")
+    )} - ${newScore}`;
   } else {
     header.textContent = "Draw!";
+    score.textContent = `${sessionStorage.getItem(
+      "p1Score"
+    )} - ${sessionStorage.getItem("p2Score")} `;
   }
 }
 
 resetbtn.addEventListener("click", () => {
-  sessionStorage.removeItem("firstLoad", true);
+  sessionStorage.clear();
   location.reload();
 });
